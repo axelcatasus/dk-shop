4af0271487e36c22b65bba45d8ce16afd627468f
axelcatasus <axel.catasus@gmail.com>
2022-12-14 01:15:24 +0100

    add welcome message

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,21 @@
 import * as React from "react"
+import { graphql } from 'gatsby'
+import { renderRichText } from 'gatsby-source-contentful/rich-text'
 import Header from '../components/Header'
 import './main.scss'
-import { main } from './index.module.scss'
+import { main, messageContainer } from './index.module.scss'
 
-const IndexPage = () => {
+const IndexPage = ({ data }) => {
+  const { allContentfulWelcomeMessage } = data
+  const message = allContentfulWelcomeMessage.edges[0].node.message
   return (
     <>
     <Header />
     <main className={main}>
       <h1>DK Shop</h1>
+      <div className={messageContainer}>
+        {renderRichText(message)}
+      </div>
     </main>
     </>
   )
@@ -16,5 +23,20 @@ const IndexPage = () => {
 }
 
 export default IndexPage
+
+export const IndexPageQuery = graphql`
+query ProductsPageQuery {
+  allContentfulWelcomeMessage {
+    edges {
+      node {
+        message {
+          raw
+        }
+        id
+      }
+    }
+  }
+}
+`
 
 export const Head = () => <title>DK Shop</title>
